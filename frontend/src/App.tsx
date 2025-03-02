526fd80b887368950298a5a261bb4a39c0c35788
Monir Hossain Showrav <rshourov156@gmail.com>
2025-03-02 18:34:37 +0600

    feat(package): tailwind css installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React Frontend</h1>
-        <p>Go Backend</p>
+        <h1 className="text-2xl font-bold text-center">React Frontend</h1>
+        <p className="text-lg text-center">Go Backend</p>
       </header>
     </div>
   );
